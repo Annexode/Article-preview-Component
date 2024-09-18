0615883a9daf911b834deac9fa64f5ea35c137a9
Annexode <annexode@gmail.com>
2024-09-18 19:51:48 +0200

    Article preview

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+        manrope: ["Manrope"],
+      },
+      colors:{
+        lightBlue:["#e5ecf4"],
+        blueGray:["#49556b"],
+      }
+    },
+  },
+  plugins: [],
+}
+
